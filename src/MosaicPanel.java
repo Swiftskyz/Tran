76274d9c032c7772dd9c2704c665401a7fb8789e
Skylar <skylarhimeno@gmail.com>
2019-05-21 08:58:14 -0600

    construct of specified number of rows and columns of rectangles

--- src/MosaicPanel.java
+++ src/MosaicPanel.java
@@ -13,3 +13,40 @@ public class MosaicPanel extends JPanel
 	private BufferedImage OSI;
 	private boolean needsRedraw;
 
+	public MosaicPanel() 
+	{
+		this(42,42,16,16);
+	}
+	
+	public MosaicPanel(int rows, int columns) 
+	{
+		this(rows,columns,16,16);
+	}
+	
+	public MosaicPanel(int rows, int columns, int preferredBlockWidth, int preferredBlockHeight) 
+	{
+		this(rows, columns, preferredBlockWidth, preferredBlockHeight, null, 0);
+	}
+	
+	public MosaicPanel(int rows, int columns, int preferredBlockWidth, int preferredBlockHeight, Color borderColor, int borderWidth) 
+	{
+		this.rows = rows;
+		this.columns = columns;
+		grid = new Color[rows][columns];
+		defaultColor = Color.black;
+		groutingColor = Color.gray;
+		alwaysDrawGrouting = false;
+		setBackground(defaultColor);
+		setOpaque(true);
+		setDoubleBuffered(false);
+		if (borderColor != null) 
+		{
+			if (borderWidth < 1)
+				borderWidth = 1;
+			setBorder(BorderFactory.createLineBorder(borderColor,borderWidth));
+		}
+		else
+			borderWidth = 0;
+		if (preferredBlockWidth > 0 && preferredBlockHeight > 0)
+			setPreferredSize(new Dimension(preferredBlockWidth*columns + 2*borderWidth, preferredBlockHeight*rows + 2*borderWidth));
+	}
